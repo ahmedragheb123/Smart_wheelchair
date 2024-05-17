a8daff59291b598dfc3b106a301284d2f08e0ba4
ahmedragheb123 <124094448+ahmedragheb123@users.noreply.github.com>
2024-05-17 17:35:40 +0300

    Add files via upload

---
+++ MPU6050/examples/MPU6050_accel_pitch_roll/MPU6050_accel_pitch_roll.ino
@@ -0,0 +1,57 @@
+/*
+    MPU6050 Triple Axis Gyroscope & Accelerometer. Pitch & Roll Accelerometer Example.
+    Read more: http://www.jarzebski.pl/arduino/czujniki-i-sensory/3-osiowy-zyroskop-i-akcelerometr-mpu6050.html
+    GIT: https://github.com/jarzebski/Arduino-MPU6050
+    Web: http://www.jarzebski.pl
+    (c) 2014 by Korneliusz Jarzebski
+*/
+
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+const int MPU = 0x68; // MPU6050 I2C address
+float AccX = 0;
+ float AccY = 0;
+ float AccZ = 0;
+  // Calculating Roll and Pitch from the accelerometer data
+ float accAngleX = 0;
+float accAngleY = 0;
+  
+void setup() 
+{
+  Serial.begin(115200);
+
+  Serial.println("Initialize MPU6050");
+
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+}
+
+void loop()
+{
+  // === Read acceleromter data === //
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
+  //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
+  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
+  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
+  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
+  // Calculating Roll and Pitch from the accelerometer data
+  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - 2.97; // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
+  accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) + 0.31; // AccErrorY ~(-1.58)
+  // Output
+  Serial.print(" roll = ");
+  Serial.print(accAngleX);
+  Serial.print(" pitch = ");
+  Serial.print(accAngleY);
+  
+  Serial.println();
+  
+  delay(10);
+}
